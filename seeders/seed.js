704b38b370f7c19a93e5e29c1a04a5198d37d818
Nilesh <das.nilesh72@gmail.com>
2024-07-11 15:03:47 +0530

    init

---
+++ seeders/seed.js
@@ -0,0 +1,30 @@
+// seed.js
+
+const { Sequelize, DataTypes } = require("sequelize");
+const { config } = require("dotenv");
+
+config();
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set");
+}
+
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
+});
+
+const Post = require("../models/post")(sequelize, DataTypes);
+
+const seedDatabase = async () => {
+  const posts = await fetch(`https://jsonplaceholder.typicode.com/posts`).then(
+    (res) => res.json()
+  );
+  posts.map(async (post) => await Post.create());
+
+  await sequelize.close();
+};
+
+seedDatabase();
